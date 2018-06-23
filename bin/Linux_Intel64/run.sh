58a28b231a73118cad4a8d0d9af035cec71e0870
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-23 10:29:16 +0900

    Modified to smaller data, added multiple VE,NP options but it is still too slow. 	modified:   HPL.dat 	modified:   run.sh

--- bin/Linux_Intel64/run.sh
+++ bin/Linux_Intel64/run.sh
@@ -2,8 +2,13 @@
 
 source /opt/nec/ve/mpi/1.1.0/bin/necmpivars.sh
 
-# Run on a single VE
-mpirun -ve 0 -np 8 ./xhpl
+# Run on a single VE and eight MPI procs
+cmd="mpirun -ve 1 -np 8 ./xhpl"
+# Run on a single VE0 and single MPI procs
+# cmd="mpirun ./xhpl"  # cannot run
 
 # Run on four VE in a chassis
-# mpirun -ve 0-3 -np 16 ./xhpl
+# cmd="mpirun -ve 0-3 -np 16 ./xhpl"
+
+echo $cmd
+$cmd
