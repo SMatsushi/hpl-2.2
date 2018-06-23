9ede2252191d59de99e11dc45f136e8df72ca6d3
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-23 10:18:54 +0900

    Run ether on a single VE or 4 VE in a chassis of A300-4. 	modified:   run.sh

--- bin/Linux_Intel64/run.sh
+++ bin/Linux_Intel64/run.sh
@@ -2,4 +2,8 @@
 
 source /opt/nec/ve/mpi/1.1.0/bin/necmpivars.sh
 
-mpirun -ve 0-3 -np 16 ./xhpl
+# Run on a single VE
+mpirun -ve 0 -np 8 ./xhpl
+
+# Run on four VE in a chassis
+# mpirun -ve 0-3 -np 16 ./xhpl
