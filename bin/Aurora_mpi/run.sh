fe348cf1ad7e52ac7a583189674f84b958831391
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-27 08:59:08 -0700

    Got 1.2TFlops (should be 1.5TFlops) with one VE 	modified:   bin/Aurora_mpi/HPL.dat Now getting proginf traces 	modified:   bin/Aurora_mpi/run.sh

--- bin/Aurora_mpi/run.sh
+++ bin/Aurora_mpi/run.sh
@@ -3,14 +3,19 @@
 source /opt/nec/ve/mpi/1.1.0/bin/necmpivars.sh
 
 # Run on a single VE and eight MPI procs
-cmd="mpirun -ve 1 -np 8 ./xhpl"
+# ave 600GFlops peak 2.5TFlops
+## cmd="mpirun -ve 1 -np 8 ./xhpl"
+
+
 # Run on a single VE0 and single MPI procs
-# cmd="mpirun ./xhpl"  # cannot run
+# using mpisep to separate proginf traces
+cmd="mpirun -ve 1 -np 8 ./mpisep.sh ./xhpl"
 
 # Run on four VE in a chassis
 # cmd="mpirun -ve 0-3 -np 16 ./xhpl"
 
 # to get Proginf output
 export VE_PROGINF=YES
+
 echo $cmd
 $cmd
